307789bf9dbceb768461818c215fdbc1f118dde4
mhmmedkhrman <lad.mutineer5252@gmail.com>
2025-07-12 09:11:28 +0300

    Add files via upload

---
+++ flutter_service_worker.js
@@ -0,0 +1,6 @@
+self.addEventListener("install", function(event) {
+  console.log("Service worker installed.");
+});
+self.addEventListener("fetch", function(event) {
+  event.respondWith(fetch(event.request));
+});
